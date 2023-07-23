cf9dc2b918d26ccd3f208548c461838fd959b599
Crhistian Alexander Garcia Urbano <42595236+crhistian2296@users.noreply.github.com>
2023-07-23 23:19:18 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -14,7 +14,27 @@ CORS(app)
 
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
+personas = [
+    {
+        "first_name": "John",
+        "age": 33,
+        "lucky_numbers": [7, 13, 22]
+    },
+    {
+        "first_name": "Jane",
+        "age": 35,
+        "lucky_numbers": [10, 14, 3]
+    },
+    {
+        "first_name": "Jimmy",
+        "age": 5,
+        "lucky_numbers": [1]
+    }
+]
 
+for p in personas:
+    jackson_family.add_member(p)
+    
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -31,14 +51,41 @@ def handle_hello():
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
     response_body = {
-        "hello": "world",
         "family": members
     }
 
 
     return jsonify(response_body), 200
 
+
+@app.route('/members/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if member: 
+        return jsonify(member), 200
+    else:
+        return jsonify({"msg": "member doesn't exit"}),400
+
+
+@app.route('/members', methods = ['POST'])
+def add_member():
+    member = request.json
+
+    if not member:
+        return jsonify({"msj": "Error"}), 400
+    jackson_family.add_member(member)
+    return jsonify({"msj":"added member"})
+
+
+app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    member = jackson_family.delete_member(member_id)
+    if not member:
+        return jsonify({"msj": "Error"})
+    return jsonify({"message": "Member deleted successfully"})
+
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
+
